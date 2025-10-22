1547cc5b8cc191c5f6d68e0eed15c247176eaef7
Daniel-Chaves-Dominguez <dchaves260299@gmail.com>
2025-10-22 10:20:45 +0200

    change system.out.println

--- src/Main.java
+++ src/Main.java
@@ -3,33 +3,37 @@
 public class Main {
     public static void main(String[] args) {
         //--------Ejercicio 1:
+        System.out.println("------Ejercicio 1------");
         String titulo = "El Resplandor";
         String autor = "Stephen King";
         int numPaginas= 688;
         mostrarLibro(titulo,autor,numPaginas);
 
         //--------Ejercicio 2:
+        System.out.println("------Ejercicio 2------");
         System.out.println(duracionMediaCanciones(120,182,240));
 
 
 
         //--------Ejercicio 3:
+        System.out.println("------Ejercicio 3------");
         System.out.println(tiempoRestanteAlquiler(20, 40));
 
 
 
         //--------Ejercicio 4:
+        System.out.println("------Ejercicio 4------");
         calcularImpuestoYDescuento(80,21,50);
 
 
 
         //--------Ejercicio 5:
+        System.out.println("------Ejercicio 5------");
         System.out.println(porcentajeVideojuegos(20, 50));
     }
 
 
     static void mostrarLibro(String titulo, String autor, int numPaginas){
-        System.out.println("------Ejercicio 1------");
         System.out.println("Título: " + titulo);
         System.out.println("Autor: " + autor);
         System.out.println("Número de páginas: " + numPaginas);
@@ -39,7 +43,6 @@ public class Main {
 
 
     static double duracionMediaCanciones(int segCancion1, int segCancion2, int segCancion3){
-        System.out.println("------Ejercicio 2------");
         double media = (double) (segCancion1 + segCancion2 + segCancion3) /3;
         return media;
     }
@@ -47,7 +50,6 @@ public class Main {
 
 
     static int tiempoRestanteAlquiler(int semanasTotales, int diasTrascurridos){
-        System.out.println("------Ejercicio 3------");
         int diasTotales = semanasTotales * 7;
         int tiempoRestante = diasTotales - diasTrascurridos;
         return tiempoRestante;
@@ -56,7 +58,6 @@ public class Main {
 
 
     static void calcularImpuestoYDescuento(double precio, int impuesto, int descuento){
-        System.out.println("------Ejercicio 4------");
         double precioConImpuesto =  precio + (precio * impuesto / 100.0);
         double precioConDescuento = precioConImpuesto - (precioConImpuesto * descuento / 100.0);
         System.out.println("Precio original: " + precio + "€");
@@ -68,7 +69,6 @@ public class Main {
 
 
     static double porcentajeVideojuegos(int numeroJuegos, int articulosTotales){
-        System.out.println("------Ejercicio 5------");
         double precioVideojuegosInventario = ((double) numeroJuegos / articulosTotales) * 100;
         return precioVideojuegosInventario;
     }
